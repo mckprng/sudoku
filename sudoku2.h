4bcc91830104260d1a54033b7093fcbe3f697a38
mckprng <mckprng@gmail.com>
2019-08-06 15:23:00 +0200

    syntax fix

--- sudoku2.h
+++ sudoku2.h
@@ -22,7 +22,7 @@ class Sudoku{
 		int bcheck(int[9][9], int, int, int);
 		int all_check(int[9][9], int, int, int);
 		int next_check(int[9][9], int, int, std::vector<int>);
-        int find_zero(int[9][9], int&, int&);
+        	int find_zero(int[9][9], int&, int&);
 
 		// Solve
 		int logic_solve(int[9][9]);
